2644128e0e8112704af54f150b723cd334010a7b
voydstack <42587659+voydstack@users.noreply.github.com>
2022-05-20 14:52:44 +0200

    Fixed RE Configuration

--- setup.py
+++ setup.py
@@ -71,15 +71,11 @@ def config_dhcp_server(interface_name):
 # Configure DNS
 def config_dns_server():
   print("[*] Configuring DNS Server")
-  with open("/etc/bind/named.conf.default-zones", "w+") as dns_config:
     dns_config.writelines([
-      "zone \"flamin.go\" {\n",
-      "\ttype master;\n",
-      "\tfile \"/etc/bind/flamin.go.db\";\n",
       "};\n"
     ])
   print("[*] Configuring domains")
-  with open("/etc/bind/flamin.go.db", "w+") as flamingo_config:
+  with open("/etc/bind/flamin.go.db", "w") as flamingo_config:
     flamingo_config.writelines([
       "$TTL	604800\n",
       "@	IN	SOA	flamin.go. root.flamin.go. (\n",
@@ -98,7 +94,7 @@ def config_dns_server():
   print("[*] Applying DNS configuration")
   os.system("named-checkconf && service bind9 restart")
   print("[*] Changing DNS resolver")
-  with open("/etc/resolv.conf", "w+") as resolv:
+  with open("/etc/resolv.conf", "w") as resolv:
     resolv.write("nameserver 127.0.0.1\n")
 
 # Configure DHCP Client
@@ -197,7 +193,6 @@ elif role in ["ra", "RA", "routeur-acces"]:
     {"name": "eth1", "ip": "120.0.1.1/24"},
     {"name": "eth2", "ip": "120.0.3.2/24"},
     {"name": "eth3", "ip": "120.0.5.1/24"},
-    {"name": "eth4", "ip": "120.0.4.1/24"}
   ])
   config_quagga()
   config_dns_server()
